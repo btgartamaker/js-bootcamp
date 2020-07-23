35948e6ecb62b6f1abe955d08dcc96c0df3d1e7f
btgartamaker <btgartamaker@gmail.com>
2020-07-22 20:35:19 -0500

    Object sections

---
+++ objects/obj-chal.js
@@ -0,0 +1,10 @@
+let person = {
+    name: 'Andrew',
+    age: 27,
+    location: 'Philidelphia'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+person.age += 1
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
